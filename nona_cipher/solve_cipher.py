23785e9076fdbca0cc702fe285f60654c06392a9
amrosnik <amrosnik@gmail.com>
2024-06-18 20:04:12 -0700

    solved the cipher in NtN...that was way too easy.

---
+++ nona_cipher/solve_cipher.py
@@ -0,0 +1,11 @@
+import string
+
+cipher = [20,8,5,20,15,23,5,18,8,1,19,18,5,1,3,20,9,22,1,20,5,4]
+
+alphabet = {(n+1):s for n,s in enumerate(string.ascii_lowercase)}
+
+message = ""
+for n,c in enumerate(cipher):
+    message += alphabet.get(c)
+
+print(message)
